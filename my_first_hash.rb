fdfdef2e75773e9bb93a7e7f517d881f49974bc4
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-04 17:19:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,11 +18,7 @@ def retrieval
                  }
                 shipping_manifest["oil paintings"]
               end
-                 
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
+                
 
 def adding
   shipping_manifest = {
